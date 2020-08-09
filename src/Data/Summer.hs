878324fc24de17c27971aa59c018339a25566c4f
Samuel Schlesinger <samuel@simspace.com>
2020-08-09 03:03:24 -0400

    changes do be happening tho

--- src/Data/Summer.hs
+++ src/Data/Summer.hs
@@ -27,11 +27,12 @@ Description: Extensible sums
 module Data.Summer
   ( -- * The extensible sum type and its associated pattern for convenience
     Sum
-  , pattern Variant
+  , pattern Inj
   -- * Construction and deconstruction of extensible sums
   , inject
   , inspect
   , consider
+  , Delete
   -- * Type family to compute the tag of a type in a type level list
   , TagIn
   , HasTagIn
@@ -57,16 +58,16 @@ import Control.Monad (unless)
 
 -- | The extensible sum type, allowing inhabitants to be of any of the
 -- types in the given type list.
-data Sum (xs :: [*]) = UnsafeVariant !Word Any
+data Sum (xs :: [*]) = UnsafeInj !Word Any
 
 type role Sum representational
 
 -- | A pattern to match on for convenience. Without this, the user facing
 -- interface is rather baroque.
-pattern Variant :: forall x xs. (x `HasTagIn` xs) => x -> Sum xs
-pattern Variant x <- (inspect -> Just x)
+pattern Inj :: forall x xs. (x `HasTagIn` xs) => x -> Sum xs
+pattern Inj x <- (inspect -> Just x)
   where
-    Variant x = inject x
+    Inj x = inject x
 
 -- | A type family for computing the tag of a given type in an extensible
 -- sum. In practice, this means computing the first index of the given type in
@@ -83,15 +84,19 @@ instance KnownNat (x `TagIn` xs) => x `HasTagIn` xs
 -- | Computes the tag of the given type in the given type level list.
 tag :: forall x xs. x `HasTagIn` xs => Word
 tag = fromInteger $ natVal (Proxy @(TagIn x xs))
+{-# INLINE CONLIKE tag #-}
 
 -- | Injects a type into the extensible sum.
 inject :: forall x xs. (x `HasTagIn` xs) => x -> Sum xs
-inject x = UnsafeVariant (tag @x @xs) (unsafeCoerce x)
+inject x = UnsafeInj (tag @x @xs) (unsafeCoerce x)
+{-# INLINE CONLIKE inject #-}
 
 -- | Inspects an extensible sum for a particular type.
 inspect :: forall x xs. (x `HasTagIn` xs) => Sum xs -> Maybe x
-inspect (UnsafeVariant tag' x) = if tag @x @xs == tag' then Just (unsafeCoerce x) else Nothing
+inspect (UnsafeInj tag' x) = if tag @x @xs == tag' then Just (unsafeCoerce x) else Nothing
+{-# INLINE CONLIKE inspect #-}
 
+-- | A type family for deleting the given type from a list
 type family Delete (x :: k) (xs :: [k]) :: [k] where
   Delete x (x ': xs) = xs
   Delete x (y ': xs) = y ': Delete x xs
@@ -100,19 +105,27 @@ type family Delete (x :: k) (xs :: [k]) :: [k] where
 -- | Consider a certain type, discarding it as an option if it is not the
 -- correct one.
 consider :: forall x xs. (x `HasTagIn` xs) => Sum xs -> Either (Sum (Delete x xs)) x
-consider (UnsafeVariant tag' x) =
+consider (UnsafeInj tag' x) =
   if tag' == tag @x @xs
     then Right (unsafeCoerce x)
-    else Left (UnsafeVariant (if tag' >= tag @x @xs then tag' - 1 else tag') x)
+    else Left (UnsafeInj (if tag' >= tag @x @xs then tag' - 1 else tag') x)
+{-# INLINE CONLIKE consider #-}
+
+-- | Consider the first type in the list of possibilities, a useful
+-- specialization for type inference.
+considerFirst :: forall x xs. Sum (x ': xs) -> Either (Sum xs) x
+considerFirst = consider
+{-# INLINE CONLIKE considerFirst #-}
 
 -- | Transforms one type in the sum into another.
 inmap :: forall x y xs. (x `HasTagIn` xs, y `HasTagIn` xs) => (x -> y) -> Sum xs -> Sum xs
-inmap f uv@(UnsafeVariant tag' x) = if tag' == tag @x @xs then UnsafeVariant (tag @y @xs) (unsafeCoerce (f (unsafeCoerce x))) else uv
+inmap f uv@(UnsafeInj tag' x) = if tag' == tag @x @xs then UnsafeInj (tag @y @xs) (unsafeCoerce (f (unsafeCoerce x))) else uv
+{-# INLINE CONLIKE inmap #-}
 
 -- | Transform one type in one sum into another type in another sum.
 smap :: forall x y xs ys. (Weaken xs ys, x `HasTagIn` xs, y `HasTagIn` ys) => (x -> y) -> Sum xs -> Sum ys
-smap f uv@(UnsafeVariant tag' x) = if tag' == tag @x @xs then UnsafeVariant (tag @y @ys) (unsafeCoerce (f (unsafeCoerce x))) else weaken uv
-
+smap f uv@(UnsafeInj tag' x) = if tag' == tag @x @xs then UnsafeInj (tag @y @ys) (unsafeCoerce (f (unsafeCoerce x))) else weaken uv
+{-# INLINE CONLIKE smap #-}
 
 -- | A class which checks that every type has the same tag in the first
 -- list as the second. In other words, checks if the first list is a prefix
@@ -125,12 +138,21 @@ instance {-# OVERLAPPABLE #-} HaveSameTagsIn xs ys => HaveSameTagsIn (x ': xs) (
 -- more possibilities at exclusively higher tags.
 noOpWeaken :: forall xs ys. (xs `HaveSameTagsIn` ys) => Sum xs -> Sum ys
 noOpWeaken = unsafeCoerce
+{-# INLINE CONLIKE noOpWeaken #-}
+
+unsafeForget :: Sum (x ': xs) -> Sum xs
+unsafeForget (UnsafeInj tag' x) = UnsafeInj (tag' - 1) x
+{-# INLINE CONLIKE unsafeForget #-}
 
 -- | Testing extensible sums for equality.
 instance (Eq (Sum xs), Eq x) => Eq (Sum (x ': xs)) where
-  UnsafeVariant tag' x == UnsafeVariant tag'' x'
-    | tag' == tag'' = if tag' == 0 then unsafeCoerce @_ @x x == unsafeCoerce @_ @x x' else UnsafeVariant @xs (tag' - 1) x == UnsafeVariant @xs (tag'' - 1) x'
+  uv@(UnsafeInj tag' x) == uv'@(UnsafeInj tag'' x')
+    | tag' == tag'' =
+        if tag' == 0
+          then unsafeCoerce @_ @x x == unsafeCoerce @_ @x x'
+          else unsafeForget uv == unsafeForget uv'
     | otherwise = False
+  {-# INLINE CONLIKE (==) #-}
 instance Eq (Sum '[]) where
   (==) = error "(==) base case: impossible by construction"
 
@@ -138,12 +160,14 @@ instance Eq (Sum '[]) where
 class Weaken xs ys where
   weaken :: Sum xs -> Sum ys
 instance {-# OVERLAPPABLE #-} (Weaken xs ys, x `HasTagIn` ys) => Weaken (x ': xs) ys where
-  weaken uv@(UnsafeVariant tag' x) =
+  weaken uv@(UnsafeInj tag' x) =
     if tag' == 0
-      then UnsafeVariant (tag @x @ys) x
-      else let UnsafeVariant tag'' _ = weaken @xs @ys (UnsafeVariant (tag' - 1) x) in UnsafeVariant tag'' x
+      then UnsafeInj (tag @x @ys) x
+      else let UnsafeInj tag'' _ = weaken @xs @ys (unsafeForget uv) in UnsafeInj tag'' x
+  {-# INLINE CONLIKE weaken #-}
 instance {-# OVERLAPPABLE #-} Weaken '[] ys where
   weaken = error "weaken base case: impossible by construction"
+  {-# INLINE CONLIKE weaken #-}
 
 -- | The scott encoding of an extensible sum
 type family Matcher xs r :: Type where
@@ -157,13 +181,16 @@ class Match xs where
 instance Match '[] where
   match = error "match base case: impossible by construction"
   override = const
+  {-# INLINE CONLIKE override #-}
 instance Match xs => Match (x ': xs) where
   match :: forall r. Sum (x ': xs) -> (x -> r) -> Matcher xs r
-  match (UnsafeVariant tag' x) f =
+  match uv@(UnsafeInj tag' x) f =
     if tag' == 0
-      then override @xs @r (f (unsafeCoerce x)) $ match @xs @r (UnsafeVariant (tag' - 1) x)
-      else match @xs @r (UnsafeVariant (tag' - 1) x)
+      then override @xs @r (f (unsafeCoerce x)) $ match @xs @r (unsafeForget uv)
+      else match @xs @r (unsafeForget uv)
+  {-# INLINE CONLIKE match #-}
   override r m = fmap (override @xs r) m
+  {-# INLINE CONLIKE override #-}
 
 test :: IO ()
 test = catchAndDisplay
@@ -184,47 +211,47 @@ test = catchAndDisplay
           tag'' = tag @Bool @'[Bool, Int]
       unless (tag' == 1) $ fail ("Tag " <> show tag' <> " does not equal 1")
     eqTest = do
-      let x :: Sum '[Int, Bool] = Variant (10 :: Int)
-          y :: Sum '[Int, Bool] = Variant True
-          z :: Sum '[Int, Bool] = Variant (11 :: Int)
+      let x :: Sum '[Int, Bool] = Inj (10 :: Int)
+          y :: Sum '[Int, Bool] = Inj True
+          z :: Sum '[Int, Bool] = Inj (11 :: Int)
           -- wrap around the alphabet like fromIntegral
-          a :: Sum '[Int, Bool] = Variant (10 :: Int)
-          b :: Sum '[Int, Bool] = Variant False
-          c :: Sum '[Int, Bool] = Variant True
+          a :: Sum '[Int, Bool] = Inj (10 :: Int)
+          b :: Sum '[Int, Bool] = Inj False
+          c :: Sum '[Int, Bool] = Inj True
       unless (x /= y) $ fail "10 equals True"
       unless (x /= z) $ fail "10 equals 11"
       unless (x == a) $ fail "10 does not equal 10"
       unless (y /= b) $ fail "True equals False"
       unless (y == c) $ fail "True does not equal True"
     noOpWeakenTest = do
-      let x :: Sum '[Int, Bool]  = Variant (10 :: Int)
+      let x :: Sum '[Int, Bool]  = Inj (10 :: Int)
           y :: Sum '[Int, Bool, Integer] = noOpWeaken x
-      unless (y == Variant (10 :: Int)) $ fail "y does not equal Variant 10"
+      unless (y == Inj (10 :: Int)) $ fail "y does not equal Inj 10"
       pure ()
     weakenTest = do
-      let x :: Sum '[Int, Bool] = Variant (10 :: Int)
+      let x :: Sum '[Int, Bool] = Inj (10 :: Int)
           y :: Sum '[Bool, Int] = weaken x
           z :: Sum '[Integer, Bool, Float, Int] = weaken y
-      unless (y == Variant (10 :: Int)) $ fail "y does not equal Variant 10"
-      unless (z == Variant (10 :: Int)) $ fail "y does not equal Variant 10"
+      unless (y == Inj (10 :: Int)) $ fail "y does not equal Inj 10"
+      unless (z == Inj (10 :: Int)) $ fail "y does not equal Inj 10"
     matchTest = do
-      let x :: Sum '[Int, Bool] = Variant (10 :: Int)
+      let x :: Sum '[Int, Bool] = Inj (10 :: Int)
       unless (match x (== 10) id) $ fail "x does not match 10"
     considerTest = do
-      let x :: Sum '[Int, Bool] = Variant (10 :: Int)
-          y :: Sum '[Int, Bool] = Variant True
+      let x :: Sum '[Int, Bool] = Inj (10 :: Int)
+          y :: Sum '[Int, Bool] = Inj True
       unless (consider @Int x == Right 10) $ fail "x at Int is not considered to be 10"
-      unless (consider @Int y == Left (Variant True)) $ fail $ "x is not considered to be Left (Variant True)"
+      unless (consider @Int y == Left (Inj True)) $ fail $ "x is not considered to be Left (Inj True)"
       unless (consider @Bool y == Right True) $ fail "x at Bool is not considered to be Right True"
     inmapTest = do
-      let x :: Sum '[Int, Bool] = Variant (10 :: Int)
+      let x :: Sum '[Int, Bool] = Inj (10 :: Int)
           y :: Sum '[Int, Bool] = inmap (== (10 :: Int)) x
           z :: Sum '[Int, Bool] = inmap (== True) x
-      unless (y == Variant True) $ fail "x did not get mapped to True"
-      unless (z == Variant (10 :: Int)) $ fail "x did not get left alone"
+      unless (y == Inj True) $ fail "x did not get mapped to True"
+      unless (z == Inj (10 :: Int)) $ fail "x did not get left alone"
     smapTest = do
-      let x :: Sum '[Int, Bool] = Variant (10 :: Int)
+      let x :: Sum '[Int, Bool] = Inj (10 :: Int)
           y :: Sum '[Bool, Int] = smap (== (10 :: Int)) x
           z :: Sum '[Bool, Int] = smap (== True) x
-      unless (y == Variant True) $ fail "x did not get mapped to True"
-      unless (z == Variant (10 :: Int)) $ fail "x did not get left alone"
+      unless (y == Inj True) $ fail "x did not get mapped to True"
+      unless (z == Inj (10 :: Int)) $ fail "x did not get left alone"
